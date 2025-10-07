77e47cd47ed4087764f663c4b0a4eb020d295a80
guisrrtt <serrettiguilherme@gmail.com>
2025-10-06 21:43:52 -0300

    Correcao final: Foto obtida do Facebook

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, Dimensions } from 'react-native';
 
-const FOTO = 'https://photos.app.goo.gl/A12kp87nxiYUdHZV8';
+const FOTO = 'https://scontent.frec3-1.fna.fbcdn.net/v/t39.30808-6/484899712_3024844144350191_5959979759299013874_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGKKrqF9rXPOt0p_HihmDEtAvp975Hp7R0C-n3vkentHffYvB9Eyz_EmpZ598p5QTD8AJZ44j87fnlkMoWwqahf&_nc_ohc=7hTGRlkBPp4Q7kNvwEtF0r2&_nc_oc=AdnNDefPPfAJyoyyZWRuTNSrUzi_RAzSKZ0gNW28gGBIhdjD3xvuat6EvA27V0X2Yuxsm_z8fHql6c2TCagh5Yp3&_nc_zt=23&_nc_ht=scontent.frec3-1.fna&_nc_gid=Kg_2EmwqZzovXoaUarF3Dw&oh=00_AfchEtk10Qy-YzJzkCtPPCdTM3jLODaUhMk5y9-VD76N4g&oe=68EA4E44';
 
 const NOME = 'Guilherme Serretti';
 const MINI_BIO = 'Sou um estudante dedicado à tecnologia, cursando Ciência da Computação. Busco sempre aprender novas habilidades e aplicá-las em projetos desafiadores para crescer profissionalmente.';
